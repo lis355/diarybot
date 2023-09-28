87e623c6f0555eb2adc008ca24945a4a4961437b
Ivan Lartsov <lartsov@gmail.com>
2023-09-28 08:38:39 +0300

    new: service/windows

---
+++ service/windows/service.js
@@ -0,0 +1,45 @@
+const path = require("node:path");
+
+const { Service } = require("node-windows");
+
+const { name } = require("../../package.json");
+
+const svc = new Service({
+	name,
+	description: "",
+	script: path.resolve("../../start.js")
+});
+
+svc.on("install", () => {
+	svc.start();
+});
+
+svc.on("alreadyinstalled", () => {
+	console.log(`${svc.name} service is already installed`);
+});
+
+svc.on("invalidinstallation", () => {
+	console.log(`${svc.name} service is invalid installed`);
+});
+
+svc.on("uninstall", () => {
+	console.log(`${svc.name} service is uninstalled`);
+});
+
+svc.on("alreadyuninstalled", () => {
+	console.log(`${svc.name} service is already uninstalled`);
+});
+
+svc.on("start", () => {
+	console.log(`${svc.name} started`);
+});
+
+svc.on("stop", () => {
+	console.log(`${svc.name} stopped`);
+});
+
+svc.on("error", error => {
+	console.error(error);
+});
+
+module.exports = svc;
